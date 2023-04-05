e88a31aa594c4dec0a1d4e855b41b30a61d67bef
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-04-05 07:51:51 +0500

    JSga Kirish | Variables | Data Types

---
+++ Lesson_001/index.js
@@ -0,0 +1,55 @@
+// let text = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab facere minima
+// laborum voluptatem nihil commodi doloremque minus impedit molestias culpa
+// veniam error voluptatum dignissimos similique iste sequi, ducimus illo, amet
+// omnis! Tenetur officiis molestias quaerat saepe laborum? Tempore illum
+// magnam adipisci eveniet obcaecati repudiandae quasi enim, temporibus error
+// nihil ducimus quos nobis ullam architecto accusantium. Minus aliquid saepe
+// vel. Nisi libero magnam laboriosam repudiandae, eius mollitia asperiores
+// voluptatem omnis ullam distinctio, harum dolor voluptatibus itaque ab quos
+// sed? Expedita a aliquid totam architecto aut veniam magnam, debitis cum
+// consequuntur autem nihil doloribus ex fuga minima ipsa voluptas recusandae
+// tempore voluptatibus.`;
+
+// console.log(text);
+// console.log("=======");
+// console.log(text);
+
+// let text;
+
+// text = "webbrain";
+
+// console.log(text);
+
+// let a = 2,
+//   b = 5;
+// console.log(a,b);
+
+// let a = (b = 1);
+// console.log(a,b);
+
+// let a;
+// let b = 2;
+// console.log(a,b);
+
+
+// let a;
+// let b = 2;
+// a=b
+
+// console.log(a,b);
+// let a = 2**53
+// console.log(a);
+
+
+// let id1 = Symbol('Webbrain')
+// let id2 = Symbol('Webbrain')
+// console.log(id1,id2);
+
+
+// let obj = {};
+// let ar = [];
+// let fun = function () {};
+
+// console.log(typeof obj);
+// console.log(typeof ar);
+// console.log(typeof fun);
--- Lesson_001/review.js
+++ Lesson_001/review.js
@@ -1,2 +1,57 @@
 // JS nima ?
-//   JSga Kirish | Variables | Data Types
+/// JS bu Html va Css da yozilgan malumotlarni harakatga keltirib beradi
+/// Js haqida malumot : 1- bo'lib 1995 yilda chiqgan va nomi liveScript bo'lgan
+/// Keyinchalik 1997 yilda EcmaScript nomiga o'zgargan
+
+// JS single thredda ishlaydi
+// singlethred bu (JS) bir vaqni o'zida faqatgina bitta ekshin bajara olishi
+
+// Variable
+/// Variable bu o'zgaruvchi malumotlarni saqlash (saxranit qilish) uchun ishlatamiz
+/// Varibale yasash uchun 3 ta keyworddan foydalanimz   yani rezorvid keyword
+/// Let Var Const
+/// rezorvdkeyword bu band qilingan sozlar
+
+// Undefined
+// bizda qandaydur o'zgaruvchi bor lekin qiymati bo'lmasa (ichi bo'sh bo'lsa) undefined qaytaradi
+
+// Data type
+/// JS da 8 ta data type bor va ular 2 guruxga bo'linadi
+/// 1-gurux Primitive  2-gurux Non primitive
+
+// Primitive ga 7ta data type kiradi ular
+// Number       // let num = 321
+// Bigint       // let bigint = 9007199254740992n
+// String       // let text = "Webbrain"
+// Booline      // let boolian = true/false
+// Null         //
+// Undefined    //
+// Symbols       // symbol bizga unik id yasab berishda ishlatiladi
+
+// NonPrimitive ga 1ta data type kiradi
+// Objects
+//objects deganda butun boshli array nazarda tutiladi
+
+//  object 3 ga bo'linadi yani ular
+/// object array function
+
+// let obj = {};
+// let ar = [];
+// let func = function () {};
+
+// console.log(typeof(obj));
+
+
+// !!!!
+// Primitive va Nanprimitive ni farqi
+
+// Primitive da call by value 
+// qachonki primitive malumotlarini chaqiradigan bo'lsak hardoim value chaqiradi
+
+// Nanprimitive da call by referince
+// qachonki Nanprimitive malumotlarini chaqiradigan bo'lsak referince qaytaradi yani link
+
+
+// Primitiveni malumotlarini chaqiradigan bo'lsak valuesi STACK ga borib joylashadi
+// NaNprimitiveni malumotlarini chaqiradigan bo'lsak linki STACK da bo'ladi uni valuesi HEAP ga borib joylashadi
+
