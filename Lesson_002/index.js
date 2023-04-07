f7b3b8d120992202b3b1e044d24ed523ec76832d
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-04-07 10:37:03 +0500

    Type Convirsions | Operators | Comparision | Logical Operators

---
+++ Lesson_002/index.js
@@ -0,0 +1,52 @@
+// let a = 1;
+// let b = "1";
+// console.log(a+b);
+
+//////////////////
+// Malumotlarni bir biriga qo'shish
+
+// let str1="webbrain"
+// let str2="academy"
+// console.log(str1+str2);
+
+//////////////////
+//NaN
+
+// let b = "1c";
+// console.log(+ b);
+
+//////////////////
+//isNaN
+
+// let b = "1c";
+// console.log(isNaN(+b));  // <-- bu NaN
+
+//////////////////
+//isNaN
+
+// let b = "1";
+// console.log(isNaN(+b));  // <-- bu NaN emas (yani not a number emas)
+
+//////////////////
+//sonlarni bir biriga qo'shish
+
+// let a = 1;
+// let b = "1";
+// console.log(a + +b);
+
+//////////////////
+//sonlarni bir biriga ko'paytirish usuli
+
+// let a = 2;
+// let b = "3";
+// console.log(a * b);
+
+//////////////////
+//sonlarni bir biridan ayirish usuli
+
+// let a = 2;
+// let b = "3";
+// console.log(+a - b);
+
+//////////////////
+
---
+++ Lesson_002/review.js
@@ -0,0 +1,15 @@
+//  Type conversion
+/// Type conversion bu malumotlarni (data typelarni) boshqa data typelarga o'tib ketishiga aytiladi
+
+
+// stringga raqam qo'shadigan bo'lsak avtomatichiski uni stringga o'rab terminalda chiqaradi
+// stringni oldidan qandaydur arifmetik amal yozadigan  bo'lsak u numberga o'zgaradi 
+// Raqam bo'lmagan malumotlarni ustida arifmetik amal bajariladigan bo'lsa bu NaN degan qiymat qaytaradi
+// NaN bu not a number degani yani raqammas bu nars degani 
+// isNaN bu berilgan parametrni NaN yoki NaN emasligini tekshirib beradi
+// prayvority bu bajarishni ustunlik tartibi
+
+
+
+
+/// arifmetik amallar
