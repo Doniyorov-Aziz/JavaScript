f798a97ebbb1a8e71908fc6a51eb596a471d2da9
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-04-08 10:09:20 +0500

    Type Convirsions | Operators | Comparision | Logical Operators

--- Lesson_002/index.js
+++ Lesson_002/index.js
@@ -49,4 +49,159 @@
 // console.log(+a - b);
 
 //////////////////
+// parseInt
 
+// let a = "13.5";
+// console.log(Number(a));
+// console.log(Number.parseInt(a));
+
+//////////////////
+// parsFloat
+
+// let b = "12.7";
+// console.log(Number(b));
+// console.log(Number.parseFloat(b));
+
+//////////////////
+// true = !0,!null,!undefined
+// true = 1  false = 0
+
+// console.log(false == 0);          // true
+// console.log(false == 1);          // false
+// console.log(false == null);       // false
+// console.log(false == undefined);  // false
+
+// console.log(true == 0);           // false
+// console.log(true == 1);           // true
+// console.log(true == null);        // false
+// console.log(true == undefined);   // false
+// console.log(null == undefined);   // true
+// console.log(true == "0");
+
+//////////////////
+//  + - * /
+
+// let a = 12;
+// let b = "15";
+
+// console.log(a + +b);
+// console.log(a - +b);
+// console.log(a * +b);
+// console.log(a / +b);
+
+//////////////////
+// % qoldiq (foiz)
+
+// console.log(123 % 25);
+// console.log(111 % 10);
+
+//////////////////
+// () qavs
+
+// console.log(1 + (1 * 1) / 1);
+// console.log(((3 + 1) * 1) / 2);
+// console.log(5 + 2 * (6 / 3));
+// console.log(5 + (4 * 7) / 2);
+
+//////////////////
+//  Icrement ++
+
+// let a=1
+// console.log(a++);
+// console.log(a);
+
+// let a=2
+// a++
+// console.log(a);
+
+// let a = 3;
+// a = a + 1;
+// console.log(a);
+
+// let a = 2;
+// console.log(a++);
+// console.log(a);
+
+// let a=5
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+// console.log(a);
+// console.log(a++);
+// console.log(a++);
+
+//////////////////
+//  Decrement --
+
+// let a=1
+// console.log(a--);
+// console.log(a);
+
+// let a=2
+// a--
+// console.log(a);
+
+// let a = 3;
+// a = a - 1;
+// console.log(a);
+
+// let a = 2;
+// console.log(a--);
+// console.log(a);
+
+// let a=5
+// console.log(a--);
+// console.log(a);
+// console.log(--a);
+// console.log(a);
+// console.log(a--);
+// console.log(a--);
+
+//////////////////
+// Obrivations
+
+// let a = 50;
+// a = a + 10;
+// a += 10;
+// console.log(a);
+
+// let a = 40;
+// a -= 20;
+// a /= 2;
+// a *= 50;
+// console.log(a);
+
+// let a = 2;
+// a += 10;
+// a *= 20;
+// a /= 5;
+// a -= 50;
+// console.log(a);
+
+//////////////////
+// Comparision
+// <,>,==,===,<=,>=,! malumotlarni compare qilish doim true yoki false qiymat qaytaradi
+
+// let a = 10;
+// let b = 12;
+// console.log(a > b); // false
+
+// let a = 3;
+// let b = 4;
+// console.log(a < b); // true
+
+// let a = 5;
+// let b = 12;
+// console.log(a == b); // false  // == bir biriga tengmi deb soraydi
+
+// let a = 20;
+// let b = 20;
+// console.log(a == b); //true 
+
+// let a = 2;
+// let b = "2";
+// console.log(a === b); // false
+
+// let a = 2;
+// let b = 2;
+// console.log(a === b); // true  // === tenglikda bir biriga tengligini va typeofigayam tengligiga qaraydi
--- Lesson_002/review.js
+++ Lesson_002/review.js
@@ -1,15 +1,32 @@
 //  Type conversion
 /// Type conversion bu malumotlarni (data typelarni) boshqa data typelarga o'tib ketishiga aytiladi
 
-
 // stringga raqam qo'shadigan bo'lsak avtomatichiski uni stringga o'rab terminalda chiqaradi
-// stringni oldidan qandaydur arifmetik amal yozadigan  bo'lsak u numberga o'zgaradi 
+// stringni oldidan qandaydur arifmetik amal yozadigan  bo'lsak u numberga o'zgaradi
 // Raqam bo'lmagan malumotlarni ustida arifmetik amal bajariladigan bo'lsa bu NaN degan qiymat qaytaradi
-// NaN bu not a number degani yani raqammas bu nars degani 
+// NaN bu not a number degani yani raqammas bu nars degani
 // isNaN bu berilgan parametrni NaN yoki NaN emasligini tekshirib beradi
 // prayvority bu bajarishni ustunlik tartibi
 
+// parseInt bu sonni butun qismini olib beradi
+// parsFloat bu sonni kasr qismini olib beradi
+// number == parsFlaot
 
+/// true = !0,!null,!undefined
+// true = 1  false = 0
 
+// Basic operators, maths
+// Addition +         // malumotni qo'shish
+// Subtraction -      // malumotni ayirish
+// Multiplication *   // malumotni ko'paytirish
+// Division /         // malumotni bo'lish
+// Remainder %        // malumotni qoldig'ini chiqarish
+// Exponentiation **  // malumotni kvadratga ko'paytirish
+// Braces ()          
+// Equality = ; == ; === ;
+// Obrivations += ; -= ; /= ; *= ;  // qisqartma shakli
+// Icrement ++
+// Decrement --
 
-/// arifmetik amallar
+// Comparision
+// <,>,==,===,<=,>=,! malumotlarni compare qilish doim true yoki false qiymat qaytaradi 
