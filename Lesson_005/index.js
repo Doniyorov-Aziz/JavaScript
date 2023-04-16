caa407d2761d5c10fc5f2db1d3664e8177e20998
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-04-16 22:46:12 +0500

    While | do while | functions.

---
+++ Lesson_005/index.js
@@ -0,0 +1,70 @@
+// let title = "webbrain"
+// for (let i = 0 ; i<title.length;i++){
+//     console.log(title[i]);
+// }
+
+// let title = "webbrain";
+// for (let i = 0; i < title.length; i++) {
+//   if (title[i] === "b") continue;
+//   console.log(title[i]);
+// }
+
+// let title = "webbrain";
+// for (let i = 0; i < title.length; i++) {
+//   if (title[i] === "b") break;
+//   console.log(title[i]);
+// }
+
+// for (let i = 0; i < 3; i++) {
+//   for (let j = 0; j < 5; j++) {
+//     console.log(j);
+//   }
+// }
+
+// for (let i = 0; i < 2; i++) {
+//   for (let j = 0; j < 5; j++) {
+//     if (j == 3) continue;
+//     console.log(j);
+//   }
+// }
+
+// outer: for (let i = 0; i < 2; i++) {
+//  inner: for (let j = 0; j < 5; j++) {
+//     if (j == 3) break outer;
+//     console.log(j);
+//   }
+// }
+
+// outer: for (let i = 0; i < 2; i++) {
+//  inner: for (let j = 0; j < 5; j++) {
+//     if (j === 3) continue outer;
+//     console.log(j);
+//   }
+// }
+
+// let num = 12344;
+// let sum = 0;
+// for (i = 0; i < 5; i++) {
+//   sum += num % 10;
+//   num = parseInt(num / 10);   // 5 xonalik sonni bir biriga qo'shish
+// }
+// console.log(sum);
+
+// let num =123276
+// let sum =0
+// while(num>0){
+//     sum += num%10
+//     num=parseInt(num/10)   // istalgan xonali sonlarni bir biriga qo'shish
+// }
+// console.log(sum);
+
+// let num = 1;
+// while (num > 0) {
+//   console.log("testing");
+//   break;
+// }
+
+// let sum = 0;
+// do {
+//   console.log("working");
+// } while (sum > 0);
---
+++ Lesson_005/review.js
@@ -0,0 +1,8 @@
+// continue bu uzidan keyin kegan narsani oqimasdan yana kodni boshiga o'tib qolishi
+// break bu malumotlarni to'xtatib qo'yish uchun ishlatiladi
+
+// outer - tashqari 
+// inner - ichkari
+
+// while bunda birinchi shartni tekshiradi kn kod yuradi
+// do while esa birinchi kod yuradi va kn shartni tekshiradi agar sharti true bolsa kod yurishni davom ettiradi agar false bo'lsa kod yurishdan to'xtaydi
