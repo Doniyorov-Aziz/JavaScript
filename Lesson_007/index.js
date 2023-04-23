da213b9cc35b3fa191dbc7731f3842748034d610
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-04-23 18:53:13 +0500

    Advanced Object | Objects - 2

---
+++ Lesson_007/index.js
@@ -0,0 +1,69 @@
+// let obj = {
+//   name: "Aziz",
+//   age: 21,
+//   child: {
+//     name: "Laziz",
+//     age: 18,
+//     child: {
+//       name: "Ozodbek",
+//       age: 11,
+//     },
+//   },
+// };
+// let sum = 0;
+// while (1) {
+//   if (obj) {
+//     sum += obj.age;
+//   } else {
+//     break;
+//   }
+//   obj = obj.child;
+// }
+// console.log(sum);
+
+//================
+
+// let user = {
+//   name: "webbrain",
+//   sayHi: function () {
+//     console.log("Hi");
+//   },
+// };
+// user.sayHi();
+
+//================
+
+// function sayHi() {
+//   console.log("Hello");
+// }
+// let user = {
+//   name: "webbrain",
+//   sayHi: sayHi,
+// };
+// user.sayHi()
+
+//================
+
+// let name = "Webbrain";
+// let obj = {
+//   name,
+//   sayHi() {
+//     console.log("I said Hi");
+//   },
+// };
+// obj.sayHi();
+
+//================
+
+// let name = "Webbrain";
+// let obj = {
+//   name,
+//   sayHi() {
+//     console.log(`Hi , ${this.name}`);
+//   },
+// };
+// obj.sayHi();
+//this parent -> obj ga teng
+
+//================
+
---
+++ Lesson_007/review.js
@@ -0,0 +1,6 @@
+// Advanced Object | Objects - 2
+
+// Garbich collection - qachonki link bo'ladigan bo'lsa objectni joylashish o'rni o'zgaradigan bo'lsa Heapdan valuesini remote qiladi yani o'chirib yuboradi
+// this objectga nisbatan ishlatiladi
+
+// Objectni ichidagi istalgan keyga access qilish uchun this dan foydalanamiz
