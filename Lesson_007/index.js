3b2573e80b04fa4f8291be08288cf844a595948f
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-04-23 20:48:47 +0500

    Advanced Object | Objects - 2

--- Lesson_007/index.js
+++ Lesson_007/index.js
@@ -55,9 +55,8 @@
 
 //================
 
-// let name = "Webbrain";
 // let obj = {
-//   name,
+//   name: "Webbrain",
 //   sayHi() {
 //     console.log(`Hi , ${this.name}`);
 //   },
@@ -67,3 +66,168 @@
 
 //================
 
+// let user = {
+//   name: "Webbrain",
+//   sayHi() {
+//     console.log(`Hi , ${this.name}`);
+//   },
+// };
+// let usr = user;
+// usr.sayHi();
+
+//================
+
+// let user = {
+//   name: "Webbrain",
+//   sayHi() {
+//     console.log(`Hi , ${this.name}`);
+//   },
+//   getName: function () {
+//     return () => {
+//       console.log(`Hello , ${this.name}`);
+//     };
+//   },
+// };
+// let usr = user;
+// usr.getName()();
+
+//================
+
+// function getName() {
+//   this.name = "Webbrain";
+//   console.log(this);
+// }
+// console.log(new getName().name);
+
+//================
+
+// function getName() {
+//   this.name = "Webbrain";
+//   this.sayHi = function () {
+//     console.log(this);
+//     return "hi";
+//   };
+// }
+// console.log(new getName().sayHi());
+
+//================
+
+// function getName(name) {
+//   this.name = name;
+//   this.sayHi = function () {
+//     console.log(`Hi`, this.name);
+//   };
+// }
+// new getName(`Aziz`).sayHi();
+// new getName(`Laziz`).sayHi();
+
+//================
+
+// let id1 = Symbol("id");
+// let id2 = Symbol("id");
+// let obj = {
+//   [id1]: "webbrian",
+// };
+// console.log(obj[id1]);
+
+//================
+
+// let obj = {
+//   name: "webbrain",
+//   title: "Frontend",
+//   age: 2020,
+// };
+// for (let i in obj){
+//     console.log(i);
+// }
+
+//================
+
+// let obj = {
+//   name: "webbrain",
+//   title: "Frontend",
+//   age: 2020,
+// };
+// for (let i in obj){
+//     console.log(obj[i]);
+// }
+
+//================
+//distructr - ajratib olish
+
+// let obj = {
+//   name: "Aziz",
+//   age: 21,
+//   title: "Frontend",
+// };
+// const { name, age, title } = obj;
+// console.log(name);
+// console.log(obj.name);    // <-- obj.name deb yozmasdan faqat name dib consolega chiqarish usuli
+
+//================
+
+// let obj = {
+//   name: "Aziz",
+//   age: 21,
+//   title: "Frontend",
+//   child: {
+//     name: "Webbrain",
+//     age: 2020,
+//   },
+// };
+// const { name, age, title, child } = obj;
+// console.log(child.age);
+
+//================
+
+// let name = "web";
+// let obj = {
+//   name: "Aziz",
+//   age: 21,
+//   title: "Frontend",
+//   child: {
+//     name: "Webbrain",
+//     age: 2020,
+//   },
+// };
+// const { name: newName, age, title, child } = obj;  // : (ikki nuqta) bunda yangi nom berishda ishlatiladi
+// console.log(child.name);
+
+//================
+
+// distructr (qirqb olish) bo'layotganda uni ichidan yana distructr qilish uchun :{} shundan foydalanamiz
+
+// let name = "web";
+// let obj = {
+//   name: "Aziz",
+//   age: 21,
+//   title: "Frontend",
+//   child: {
+//     childname: "Webbrain",
+//     childage: 2020,
+//   },
+// };
+// const { name: newName, age, title, child:{childname,childage} } = obj;
+// console.log(childname);
+
+//================
+//sprite opirator --> ...  ( 3ta nuqta)
+
+// let obj = {
+//   child: {
+//     name: "webbrain",
+//     age: 2020,
+//   },
+// };
+// console.log(obj);
+// obj = { ...obj.child };
+// console.log(obj);
+
+//================
+
+// const obj = {
+//   name: "webbrain",
+//   age: 2020,
+// };
+// console.log({ ...obj, name: "It center" });
+// sprite operatori bilan name ni qiymatini o'zgartirish 
--- Lesson_007/review.js
+++ Lesson_007/review.js
@@ -4,3 +4,60 @@
 // this objectga nisbatan ishlatiladi
 
 // Objectni ichidagi istalgan keyga access qilish uchun this dan foydalanamiz
+// new.target - constraktor bilan funksiya chaqirilgan yoki chaqirilmaganligini aniqlashtirib beradi
+
+//==============
+
+// symbols ni qiymatiyam objectni keysi bo'la oladi faqatgina []shuni ichida yoziladi
+// let id1 = Symbol("id");
+// let id2 = Symbol("id");
+
+// let obj = {
+//     [id1] : "webbrain"
+// }
+// console.log(obj[id1]);
+
+//==============
+
+//distructr - ajratib olish
+
+// let obj = {
+//     name: "Aziz",
+//     age: 21,
+//     title: "Frontend",
+//   };
+//   const { name, age, title } = obj;
+//   console.log(name);
+//   console.log(obj.name);  // <-- obj.name deb yozmasdan faqat name dib consolega chiqarish usuli
+
+//==============
+
+// let name = "web";
+// let obj = {
+//   name: "Aziz",
+//   age: 21,
+//   title: "Frontend",
+//   child: {
+//     name: "Webbrain",
+//     age: 2020,
+//   },
+// };
+// const { name: newName, age, title, child } = obj; // : (ikki nuqta) bunda yangi nom berishda ishlatiladi
+// console.log(child.age);
+
+//==============
+
+// distructr (qirqb olish) bo'layotganda uni ichidan yana distructr qilish uchun :{} shundan foydalanamiz 
+
+// let name = "web";
+// let obj = {
+//   name: "Aziz",
+//   age: 21,
+//   title: "Frontend",
+//   child: {
+//     childname: "Webbrain",
+//     childage: 2020,
+//   },
+// };
+// const { name: newName, age, title, child:{childname,childage} } = obj;
+// console.log(childname);
