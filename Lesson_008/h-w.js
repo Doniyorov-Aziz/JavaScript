3d31a3f629458b580659aabbf9a8c8cd4c4654e3
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-04-26 00:00:47 +0500

    Numbers | All you need to know

--- Lesson_008/h-w.js
+++ Lesson_008/h-w.js
@@ -1,6 +1,29 @@
-
 // console.log((Math.max(3, 2, 5, 13, 54, 23)) - (Math.min(3, 2, 5, 13, 54, 23)));
 
+//=============
 
+// benaryga o'tgan sonni ichida nechta 1 va nechta 0 qatnashganini biladigan kod
+
+// function getBynry(num) {
+//   let byd = num.toString(2);
+//   let bir = "";
+//   let nol = "";
+//   for (i in byd) {
+//     switch (byd[i]) {
+//       case "1":
+//         bir += "1";
+//         break;
+//       case "0":
+//         nol += "0";
+//         break;
+//     }
+//   }
+//   console.log(byd);
+//   console.log(`1 dan ${bir.length} ta qatnashgan : ${bir}`);
+//   console.log(`0 dan ${nol.length} ta qatnashgan : ${nol}`);
+// }
+// getBynry(53);
+
+//=============
 
 
