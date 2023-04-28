57ad8abec59748fee564c2c88f00b67ceffa2fa0
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-04-28 17:48:56 +0500

    String | All you need to know about String

--- Lesson_009/index.js
+++ Lesson_009/index.js
@@ -1,4 +1,223 @@
-let str1 = "webbrain 'Academiy'";
-let str2 = '"webbrain" Academiy';
-let str3 = `webbrain Academiy`;
-console.log(str1, str2, str3);
+// let str1 = "1 webbrain 'Academiy'";
+// let str2 = '"2 webbrain" Academiy';
+// let str3 = `3 webbrain
+// ${str1}
+//  Academiy`;
+
+// console.log(str3);
+
+//===============
+
+// let str1 = "webbrain";
+// let str2 = new String("webbrain");
+
+// console.log(str1 == "webbrain");  // true
+// console.log(str2 == "webbrain");  // true
+// console.log(str2 === "webbrain"); // false
+// console.log(str2); // [String: 'webbrain']
+
+//===============
+//localeCompare() 2 ta stringni taqqoslash   (-1;0;1)
+
+// let str1 = "webbrain";
+// let str2 = new String("webbrain");
+
+// console.log(str1.localeCompare(str2));
+
+//===============
+
+// let str1 = "webbrain"
+// console.log(str1[3]);             // b
+// console.log(str1[-3]);            // undefined
+// console.log(str1.at(2));          // b
+// console.log(str1.at(-2));         // i
+// console.log(str1.charAt(2));      // b
+// console.log(str1.charAt(-2));     // bo'sh joy (sababi charAt minus qiymat qabul qilmaydi)
+
+//===============
+// length
+
+// let str = "webbrain"
+// console.log(str.length); // 8
+
+// let str = "webbrain academy"
+// console.log(str.length);  // 16   // bo'sh joyniyam inobatga oladi
+
+//===============
+// at
+
+// let str = "webbrain"
+// console.log(str.at(0));   // w
+// console.log(str.at(-1));  // n
+
+//===============
+// toUpperCase
+
+// let str = "webbrain"
+// console.log(str.toUpperCase());  // WEBBRAIN
+
+// let str = "webbrain"
+// console.log(`${str}`.toUpperCase());
+
+// `${ }` bu malumotni hardoim stringda deb qabul qilishi uchun shu ko'rinishda yozamiz
+
+//===============
+// toLowerCase
+
+// let str = "WEBbrain"             // webbrain
+// console.log(str.toLowerCase());
+
+// let str = "WEBbraiN1231"             // webbrain1231
+// console.log(`${str}`.toLowerCase());
+
+//===============
+// toLocaleLowerCase bu qaysi til bo'yicha so'zni kichraytirish va o'zining default holati inglizcha
+
+// let str= "WebbrAin"
+// console.log(str.toLocaleLowerCase());
+
+//===============
+// toLocaleUpperCase bu qaysi til bo'yicha so'zni kattartirish va o'zining default holati inglizcha
+
+// let str= "WebbrAin"
+// console.log(str.toLocaleUpperCase());
+
+//===============
+// indexOf
+
+// let str = "webbrain academy"
+// console.log(str.indexOf("webbr")); // 0
+
+// let str = "webbrain academy"
+// console.log(str.indexOf("webbrain academy")); // 0
+
+// let str = "webbrain academy"
+// console.log(str.indexOf("webkasdnai"));  // -1
+
+// let str = "webbrain academy"
+// console.log(str.indexOf(" "));  // 8   // sababi stringni ichida bo'sh joy bor
+
+// let str = "webbrain academy"
+// console.log(str.indexOf("bb")); // 2  // sababi 2- indexdan bowlab bb bor
+
+// let str = "webbrain academy";
+// console.log(str.indexOf("bb", 5));  // -1   // yani xato 5- indexdan boshlab bb harf qatnashganmi deb sorepti yo'q bo'lgani uchun -1 qaytaradi
+// ikkinchi parametr nechinchi indexdan boshlab qidirish kerakligini ko'rsatadi
+
+//===============
+// lastIndeexOf
+
+// let str = "webbrain acadebbmy";
+// console.log(str.indexOf("bb"));       // 2
+// console.log(str.lastIndexOf("bb"));   // 14
+
+//===============
+// includes
+
+// let str = "webbrain academy"
+// console.log(str.includes("in"));
+
+//===============
+// startsWith
+
+// let str = "webbrain academy"
+// console.log(str.startsWith("in"));
+
+//===============
+// endsWith
+
+// let str = "webbrain academy"
+// console.log(str.endsWith("in"));  // false
+
+// let str2 = "webbrain academy"
+// console.log(str2.endsWith("emy")); // true
+
+//===============
+//padStart
+
+// let str = "webbrain academy"
+// console.log(str.padStart(24,"."));
+
+//===============
+//padEnd
+
+// let str = "webbrain academy"
+// console.log(str.padEnd(24,"."));
+
+//===============
+// slice
+
+// let str = "webbrain academy"
+// console.log(str.slice(3,6)); // bra
+
+// let str = "webbrain academy"
+// console.log(str.slice(0,-2)); // webbrain acade
+
+//===============
+// substring
+
+// let str = "webbrain it academy";
+// console.log(str.substring(0, 3)); // web
+
+// let str = "webbrain it academy";
+// console.log(str.substring(3, 0));  // logica jihatdan to'g'ri kelmaydigan buyruq berilgan va ozi svipe(svayp) qilib olgan
+
+//===============
+// substr       // dublicate bo'b ketishi mumkin  va qulaylik jihati hohlagan indexdan boshlab hohlagan indexgacha qirqib olsa bo'ladi
+
+// let str = "webbrain it academy";
+// console.log(str.substr(3, 3));  // bra
+
+// let str = "webbrain it academy";
+// console.log(str.substr(6, 12));  // in it academ
+
+//===============
+// eval  bu string ichidagi arifmetik amallarni bajaradi
+
+// console.log(eval(2 * 3 + 5 / 5));  // 7
+// console.log(eval(((125 / 5) * 10) / 5 + 20)); // 70
+
+//===============
+// trim
+
+// let str = "    webbrain academy   "
+// console.log(str);          //     webbrain academy
+// console.log(str.trim());  // webbrain academy
+
+//===============
+// split
+
+// let str = "webbrain academy"
+// console.log(str.split(" "));   // 'webbrain' , 'academy' // bo'sh joy bolsa alohida elementga o'tkazib beradi
+
+// let str = "webbrain academy"
+// console.log(str.split(""));   // 'w', 'e', 'b', 'b', 'r', 'a' ,'i', 'n', 'a', 'c', 'a', 'd', 'e', 'm', 'y' // bo'sh joy bo'lmasa har bir harfini stringga o'tkazib beradi
+
+// let str = "webbrain academy"
+// console.log(str.split("a")); // 'webbr', 'in ', 'c','demy'   //  agar a harfini ko'rganda desak a harfi bor indexni alohida elementga o'tkazib beradi
+
+//===============
+// concat (concationation)
+
+// let str = "webbrain"
+// console.log(str.concat(" academy"));
+
+// let str = "webbrain"
+// console.log(str.concat(" academy" , " It" , " center"));
+
+//===============
+// valeuOf  // new stringni valuesini olish
+
+// console.log(new String("webbrain").valueOf());
+
+//===============
+// replace
+
+// let str = "web brain academy"
+// console.log(str.replace("web","Web"));
+
+// let str = "web brain web academy";
+// console.log(str.replace(/Web/i, "WEB"));  
+
+// let str = "web brain web academy"
+// console.log(str.replace(/Web/gi,"WEB"));  
--- Lesson_009/review.js
+++ Lesson_009/review.js
@@ -1,1 +1,51 @@
 // String | All you need to know about String
+
+// constraktor bizga doim objectni qaytaradi
+// 2 ta stringni birbiriga taqqoslash uchun localeCompare() bilan taqqoslaymiz
+// localeCompare() bizga -1 , 0 , 1 qaytaradi 
+// -1 umuman o'xshamaydi  , 0 100% bir biriga teng , 1 qisman to'g'ri
+
+//===============
+
+// index[] doim 0 dan boshlanadi  va minus qiymat qabul qilmaydi
+// charAt() bu index ni chiqarib beradi va minus qiymat qabul qilmaydi
+//at() bu indexni chiqarib beradi va minus qiymatam qabul qiladi
+
+// let str1 = "webbrain"
+// console.log(str1[3]);             // b
+// console.log(str1[-3]);            // undefined
+// console.log(str1.at(2));          // b
+// console.log(str1.at(-2));         // i
+// console.log(str1.charAt(2));      // b
+// console.log(str1.charAt(-2));     // bo'sh joy (sababi charAt minus qiymat qabul qilmaydi)
+
+//===============
+// String Metods
+
+// length bu stringni uzunligini chiqarib beradi va hattoki bo'sh joyni ham inobatga oladi
+// at() bu harfni chiqarib beradi va minus qiyamtniyam qabul qiladi
+// toUpperCase() bu harflarni barini katta harfga o'tkazib beradi
+// toLowerCase() bu harflarni barini kichkina harfga o'tkazib beradi
+// toLocaleUpperCase bu qaysi til bo'yicha so'zni kattartirish va o'zining default holati inglizcha
+// toLocaleLowerCase bu qaysi til bo'yicha so'zni kichraytirish va o'zining default holati inglizcha
+// indexOf() bu malumotimz bir xil va qisman to'gri bo'ladigan bo'lsa 0 qaytadi , xato bo'lsa -1 , " " stringda bo'sh joy bo'ladigan bo'lsa 8 qayatdi
+// lastIndexOf() bu malumotni o'xshashligini oxiridan boshlab qidiradi 
+// includes() biz yozgan malumot bor yoki yoqligini qidirb beradu va true va false qaytaradi
+// startsWith() malumot haqqatdanam biz yozgan so'zdan boshlanadimi boshlansa true boshlanmasa false
+// endsWith  malumot haqqatdanam biz yozgan so'z bilan tugaydimi, tugasa true tugamasa false
+// padStart() malumotga boshidan nechta so'z qo'shilishi
+// padEnd() malumotga oxiridan nechta so'z qo'shilishi
+// slice() bu malumotlarni qirqb olish uchun ishlatiladi , 2 ta qiymat qabul qiladi va pilus minusli qiymatni ham qabul qiladi
+// subsstring () bu minusli qiymat qabul qilmaydi , bu ham malumotni qirqb olish uchun ishlatiladi va logika jihatidan to'g'ri kelmaydigan buyruq berilsa o'zi aftomaticski svipe(svayp) qib oladi
+// substr ()  // dublicate bo'b ketishi mumkin  va qulaylik jihati hohlagan indexdan boshlab hohlagan indexgacha qirqib olsa bo'ladi
+// eval bu string ichidagi arifmetik amallarni bajaradi
+// trim() bu malumotlarni boshi va oxirisidagi ortiqcha bo'sh joylarni ignor qivoradi
+// trimStart() bowidagi bo'sh joylarni o'chirib tashlaydi
+// trimEnd() oxiridagi bo'sh joylarni o'chirib tashlaydi
+// split bu malumotlarni arrayga o'tkazib beradi
+// concat (concationation) bu 2 ta stringni bir biriga qo'shib beradi va vergil bilan ixtiyory so'zni qo'shib ketsak bo'ladi
+// new Stringni faqat gina valeusini olish uchun valueOf() dan foydalanamiz
+// replace() bu string ichidagi malumotni o'zgartirmoqchi bo'lsak foydalanamiz va u 2 ta qiymat qabul qiladi 1-si o'zgartirmoqchi bo'lgan qiyamt 2-si yangi qiymat
+
+
+
