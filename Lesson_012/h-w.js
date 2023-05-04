f43cf7e3d3987c8e8202869ee6d18ed90c914b8d
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-05-04 16:46:11 +0500

    Advanced Array CRUD exercise - 3/3

--- Lesson_012/h-w.js
+++ Lesson_012/h-w.js
@@ -71,14 +71,86 @@
 // };
 // console.log(obj);
 
-// #7
+// #7  nechta harf qatnashganini chiqarish
 
-let str = "Webbrain Academy";
-let res = {};
-function getCount(text) {
-  for (i of text) {
-    res[i] ? (res[i] = res[i] + 1) : (res[i] = 1);
-  }
-  console.log(res);
-}
-getCount(str)
+// let str = "Webbrain Academy";
+// let res = {};
+// function getCount(text) {
+//   for (i of text) {
+//     res[i] ? (res[i] = res[i] + 1) : (res[i] = 1);
+//   }
+//   console.log(res);
+// }
+// getCount(str)
+
+// #8  berilgan  malumotni har bir harfini 2 tadan qilib beradi
+
+// let str = "webbrain";
+// let res = "";
+// Array.from(str, (v) => {             // 1-usul
+//   res += v + v;
+// });
+// console.log(res);
+
+// let str = "Webbrain";
+// let res = "";
+// for (let i of str) {                 // 2-usul
+//   res += i + i;
+// }
+// console.log(res);
+
+// #9 malumot ichidagi sonlarni toping
+
+// let str = "we66ra1n a1c32ad4my";
+// let res = "";
+// for (let i of str){                   // 1-usul
+//     if(+i){
+//         res += i
+//     }
+// }
+// console.log(res);
+
+// let str = "we66rain acade4y";        // 2-usul
+// let res = "";
+// for (let i of str) {
+//   if (i.toUpperCase() == i.toLowerCase() && i != ` `) {
+//     res += i;
+//   }
+// }
+// console.log(res);
+
+// #10  2 ta stringni ichidagi harflar soni bir biriga tengligini tekshirish kerak
+
+// let str1 = "web";
+// let str2 = "ewb";
+// function data(str1, str2) {
+//   let res = str1.split("");
+//   let res2 = str2.split("");
+//   res.sort((a, b) => a.localeCompare(b));
+//   res2.sort((a, b) => a.localeCompare(b));
+//   res = res.toString();
+//   res2 = res2.toString();
+//   console.log(res == res2);
+// }
+// data(str1, str2);
+
+// #11
+
+// let str = "webbraaain";
+// let obj = "";
+// let count=0
+// for (let i in str) {
+//   if(obj[obj.length-1]===str[i]){
+//     count++
+//   }else {
+//     obj+=str[i]
+//   }
+// }
+// console.log(obj);
+
+// #12
+
+// let obj = { title: "webbrain", age: 2020 };
+// for (let i in obj) {
+//   console.log(i);
+// }
