a8f1aa9cf04f5b0734b182613870510cbed9fa9e
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-05-06 22:45:14 +0500

    Datastructure| new Date() | JSON

--- Lesson_013/index.js
+++ Lesson_013/index.js
@@ -1,3 +1,11 @@
 // let obj = { name: "webbrain", title: "It center" };
 // let { name, title } = obj;
-// console.log(name,obj.name);
+// console.log(name, obj.name);
+
+function getReverse(a) {
+  let res = a.split("");
+  console.log(res.reverse().join(""));
+}
+getReverse("webbrain");
+
+
--- Lesson_013/review.js
+++ Lesson_013/review.js
@@ -1,1 +1,21 @@
 // Datastructure| new Date() | JSON
+
+// const getZiplash = (str) => {
+//   let res = "";
+//   let zip = 1;
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === str[i + 1]) {
+//       zip++;
+//     } else {
+//       res += str[i];
+//       if (zip > 1) {
+//         res += zip;
+//         zip = 1;
+//       }
+//     }
+//   }
+//   console.log(res);
+// };
+// getZiplash("wwebbbbbrrrraaiiiinnn");
+// let res
+//
