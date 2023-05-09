d4e636cca22d301838466103e220c9afeb494ae0
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-05-09 23:10:44 +0500

    new Map| new Set | new WeakMap | new WeakSet

---
+++ Lesson_014/index.js
@@ -0,0 +1,38 @@
+// let obj = {};
+// let map = new Map();
+
+// obj.name = "webbrain";
+// map.name = "webbrain";
+
+// console.log(obj, obj.name);
+// console.log(map, map.name);
+
+//==========================
+// map.set()
+
+// let map = new Map()
+
+// map.set("name","It center")
+// console.log(map);
+// console.log(map.get("name"));  // mapni ichidagi valuesini olish uchun map.get() dan foydalanamiz
+
+//==========================
+// map.has()   biz izlagan malumotni mapni ichida bor yo'qligini tekshiradi va true false qaytaradi
+
+// let map = new Map()
+
+// map.set("name","It center")
+// console.log(map.get("name")); // It center
+// console.log(map.has("name")); // true
+
+//==========================
+// map.delete() bu malumotni o'chirish uchun ishlatamiz
+
+// let map = new Map();
+
+// map.set("name", "webbrain");
+// map.set("title", "It center");
+// map.set("founded", 2020);
+
+// console.log(map.delete("title"));
+// console.log(map);
---
+++ Lesson_014/review.js
@@ -0,0 +1,6 @@
+// new Map| new Set | new WeakMap | new WeakSet
+
+// new set bu advanced key collection value hisoblanadi
+// mapni ichidagi valuesini olishimiz uchun map.get() ishlatamiz va qavsni ichiga bizga kerakli bo'lgan valueni keysini yozamiz
+// map.has() bu mapni ichida biz izlagan malumot bor yo'qligini tekshiradi va bizga true false qaytaradi
+// map.delete() bu malumotni o'chirish uchun ishlatamiz 
