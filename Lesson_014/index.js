5dc8c3ecd81fbd8aef4fc529495fb30b3699f2c8
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-05-10 11:39:58 +0500

    new Map| new Set | new WeakMap | new WeakSet

--- Lesson_014/index.js
+++ Lesson_014/index.js
@@ -36,3 +36,93 @@
 
 // console.log(map.delete("title"));
 // console.log(map);
+
+//==========================
+// map.clear()   bu parametr qabul qilmaydi va mapni ichidagi bari keylarni o'chirib yuboradi
+
+// let map = new Map();
+
+// map.set("name", "webbrain");
+// map.set("title", "frontend");
+// map.set("foundend", 2020);
+// console.log(map.clear());
+// console.log(map);
+
+//==========================
+// keys,values,entries
+
+// let map = new Map([
+//   ["name", "webbrain"],
+//   ["title", "it center"],
+// ]);
+// console.log(map.keys());
+// console.log(map.values());
+// console.log(map.entries());
+
+//==========================
+// map bn for of ni ishlatish
+
+// let map = new Map([
+//   ["name", "webbrain"],
+//   ["title", "it center"],
+// ]);
+
+// for (let vl of map.keys()) {    // name , title
+//   console.log(vl);
+// }
+
+// for (let vl of map.values()) {    // webbrain , it center
+//   console.log(vl);
+// }
+
+// for (let vl of map.entries()) {
+//   console.log(vl);
+// }
+
+//==========================
+// map.next()
+
+// let map = new Map([
+//   ["name", "webbrain"],
+//   ["title", "it center"],
+// ]);
+
+// let gen = map.entries();
+// gen.next();
+// console.log(gen.next());
+
+//==========================
+// forEach
+
+// let map = new Map([
+//   ["name", "webbrain"],
+//   ["title", "it center"],
+// ]);
+
+// map.forEach((element) => {
+//   console.log(element);
+// });  // webbrain , it center
+
+//==========================
+// Object,fromEntries(map)  mapni odatiy objectga o'tkazish
+
+// let map = new Map([
+//   ["name", "webbrain"],
+//   ["title", "it center"],
+// ]);
+// console.log(Object.fromEntries(map)); 
+
+// let map = new Map([
+//   ["name", "webbrain"],
+//   ["title", "it center"],
+// ]);
+// console.log(Object.fromEntries(map).title);   // it center 
+
+//==========================
+// Set
+
+// let set = new Set()
+
+// set.add("toshmat")
+// set.add("eshmat")
+// console.log(set);
--- Lesson_014/review.js
+++ Lesson_014/review.js
@@ -4,3 +4,8 @@
 // mapni ichidagi valuesini olishimiz uchun map.get() ishlatamiz va qavsni ichiga bizga kerakli bo'lgan valueni keysini yozamiz
 // map.has() bu mapni ichida biz izlagan malumot bor yo'qligini tekshiradi va bizga true false qaytaradi
 // map.delete() bu malumotni o'chirish uchun ishlatamiz 
+// map.clear() bu parametr qabul qilmaydi va mapni ichida qanaqa key bo'ladigan bo'lsa barini o'chirib yuboradi
+// map ni odatiy objectga o'tkazish uchun Object.fromEntries(map) dan foydalanamiz 
+
+// map bu key va value collection  va map objectga o'xshaydi
+// set faqatgina value collection va set arrayga o'xshaydi
