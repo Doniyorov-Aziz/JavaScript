7f53bacaed8e609b6c10607547619834c6ac68a1
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-05-12 12:06:22 +0500

    Recursion function

---
+++ Lesson_015/index.js
@@ -0,0 +1,28 @@
+// funcsiyaga qiymat bersak shungacha bo'lgan sonlari yig'indisini chiqarish
+
+// 1-usul
+
+// const sum = (n) => {
+//   let s = 0;
+//   for (let i = 0; i <= n; i++) {
+//     s += i;
+//   }
+//   console.log(s);
+// };
+// sum(7) // 7+6+5+4+3+2+1 = 28
+
+// 2-usul
+
+// let res = 0;
+// const sum = (n) => {
+//   if (n === 1) return (res = res + 1);
+//   else {
+//     res += n;
+//     return sum(n - 1);
+//   }
+// };
+// sum(7);
+// console.log(res); // 28
+
+//==================================
+
